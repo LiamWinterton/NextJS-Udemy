a4ea5a9fd81396da86a5f73bda44b542a7e4993b
Liam <liam@dijitul.uk>
2021-03-23 17:50:42 +0000

    Navigating programatically using router.push

--- pages/clients/[clientID]/index.js
+++ pages/clients/[clientID]/index.js
@@ -1,11 +1,26 @@
 import { useRouter } from 'next/router'
 
-export default function ClientSingle() {
+function ClientSingle() {
 	const router = useRouter()
+
+	function handleLoadButtonPress() {
+		// Do some logic
+		// router.push(`/clients/${router.query.clientID}/projectA`)
+		router.push({
+			pathname: '/clients/[clientID]/[projectID]',
+			query: {
+				clientID: router.query.clientID,
+				projectID: "project-1"
+			}
+		})
+	}
 
 	return (
 		<div>
-			<h1>{router.query.clientID}</h1>
+			<h1>Client: {router.query.clientID}</h1>
+			<button onClick={handleLoadButtonPress}>Load Project 1</button>
 		</div>
 	)
 }
+
+export default ClientSingle
