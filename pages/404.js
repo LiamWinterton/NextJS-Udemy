c72efcdd5549dca6cb19a22e9d86cd806e4c5874
Liam <liam@dijitul.uk>
2021-03-23 17:52:39 +0000

    Custom 404 page!

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound404() {
+	return (
+		<div>
+			<h1>Page not found!</h1>
+			<p>Somethings wrong, I can feel it....</p>
+		</div>
+	)
+}
+
+export default NotFound404
